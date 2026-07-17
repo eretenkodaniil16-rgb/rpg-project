312179c91582b2816c3f06c6ffd0174a25c2008a
eretenkodaniil16-rgb <eretenkodaniil16@gmail.com>
2026-07-17 13:16:00 +0300

    test: validate public mobile movement interface

--- tests/smoke_player_runtime.gd
+++ tests/smoke_player_runtime.gd
@@ -32,10 +32,14 @@ func _run_smoke_test() -> void:
 	assert(body.color.is_equal_approx(Color.from_string("#9368D8", Color.WHITE)))
 
 	player.call("set_mobile_vector", Vector2.RIGHT)
-	var mobile_direction: Vector2 = player.call("_get_mobile_direction") as Vector2
+	var direction_value: Variant = player.call("get_mobile_direction")
+	assert(direction_value is Vector2)
+	var mobile_direction: Vector2 = direction_value
 	assert(mobile_direction.is_equal_approx(Vector2.RIGHT))
 	player.call("clear_mobile_input")
-	assert((player.call("_get_mobile_direction") as Vector2).is_zero_approx())
+	var cleared_value: Variant = player.call("get_mobile_direction")
+	assert(cleared_value is Vector2)
+	assert((cleared_value as Vector2).is_zero_approx())
 
 	print("Player runtime smoke test passed.")
 	game.queue_free()
