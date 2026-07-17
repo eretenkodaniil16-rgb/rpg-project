5c54ee056e3c846d3f2288c4c01c5bbd8a538a89
eretenkodaniil16-rgb <eretenkodaniil16@gmail.com>
2026-07-17 13:50:44 +0300

    test: load virtual joystick script directly

--- tests/test_virtual_joystick.gd
+++ tests/test_virtual_joystick.gd
@@ -1,19 +1,21 @@
 extends SceneTree
 
+const JOYSTICK_SCRIPT: Script = preload("res://scripts/ui/virtual_joystick.gd")
+
 
 func _init() -> void:
-	var inside_dead_zone: Vector2 = VirtualJoystick.calculate_output_vector(Vector2(0.1, 0.0), 0.16)
+	var inside_dead_zone: Vector2 = JOYSTICK_SCRIPT.calculate_output_vector(Vector2(0.1, 0.0), 0.16)
 	assert(inside_dead_zone.is_zero_approx())
 
-	var full_right: Vector2 = VirtualJoystick.calculate_output_vector(Vector2.RIGHT, 0.16)
+	var full_right: Vector2 = JOYSTICK_SCRIPT.calculate_output_vector(Vector2.RIGHT, 0.16)
 	assert(full_right.is_equal_approx(Vector2.RIGHT))
 
-	var diagonal: Vector2 = VirtualJoystick.calculate_output_vector(Vector2(0.8, -0.8), 0.16)
+	var diagonal: Vector2 = JOYSTICK_SCRIPT.calculate_output_vector(Vector2(0.8, -0.8), 0.16)
 	assert(diagonal.x > 0.0)
 	assert(diagonal.y < 0.0)
 	assert(diagonal.length() <= 1.001)
 
-	var partial: Vector2 = VirtualJoystick.calculate_output_vector(Vector2(0.5, 0.0), 0.16)
+	var partial: Vector2 = JOYSTICK_SCRIPT.calculate_output_vector(Vector2(0.5, 0.0), 0.16)
 	assert(partial.x > 0.0 and partial.x < 1.0)
 	assert(is_zero_approx(partial.y))
 
