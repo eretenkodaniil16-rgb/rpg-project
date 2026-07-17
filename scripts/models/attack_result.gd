a7ba8c1e22fe09ff6ceed46b6e820419426097bd
eretenkodaniil16-rgb <eretenkodaniil16@gmail.com>
2026-07-17 17:37:25 +0300

    feat: generalize attack results for weapons and spells

--- scripts/models/attack_result.gd
+++ scripts/models/attack_result.gd
@@ -1,6 +1,11 @@
 class_name AttackResult
 extends RefCounted
 
+var attack_name: String = "Безоружный удар"
+var ability_name: String = "Сила"
+var damage_type: String = "дробящий"
+var is_spell: bool = false
+var automatic_hit: bool = false
 var natural_roll: int = 1
 var ability_modifier: int = 0
 var proficiency_bonus: int = 0
@@ -11,12 +16,19 @@ var hit: bool = false
 var critical: bool = false
 var automatic_miss: bool = false
 var damage: int = 0
+var bonus_damage: int = 0
 var target_health_after: int = 0
 var target_max_health: int = 0
+var note: String = ""
 
 
 func to_dict() -> Dictionary:
 	return {
+		"attack_name": attack_name,
+		"ability_name": ability_name,
+		"damage_type": damage_type,
+		"is_spell": is_spell,
+		"automatic_hit": automatic_hit,
 		"natural_roll": natural_roll,
 		"ability_modifier": ability_modifier,
 		"proficiency_bonus": proficiency_bonus,
@@ -27,6 +39,8 @@ func to_dict() -> Dictionary:
 		"critical": critical,
 		"automatic_miss": automatic_miss,
 		"damage": damage,
+		"bonus_damage": bonus_damage,
 		"target_health_after": target_health_after,
-		"target_max_health": target_max_health
+		"target_max_health": target_max_health,
+		"note": note
 	}
