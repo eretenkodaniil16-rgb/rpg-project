ce1856d1f8332e4afa8a15f82925e42bc70268f7
eretenkodaniil16-rgb <eretenkodaniil16@gmail.com>
2026-07-27 21:34:37 +0300

    fix: сохранять имя PreparedActionPanel в игровом runtime

--- scripts/game/game_presented_rolls.gd
+++ scripts/game/game_presented_rolls.gd
@@ -2,11 +2,13 @@ extends "res://scripts/game/game_roll_presentation.gd"
 
 const PRESENTING_SRD_RULES: Script = preload("res://scripts/systems/presenting_srd_combat_rules.gd")
 
+
 func _ready() -> void:
 	_srd_rules = PRESENTING_SRD_RULES.new() as SrdCombatRules
 	super._ready()
 	if _ability_panel != null:
-		_ability_panel.name = "AbilityPanel"
+		_ability_panel.name = "PreparedActionPanel"
+
 
 func _process(delta: float) -> void:
 	super._process(delta)
