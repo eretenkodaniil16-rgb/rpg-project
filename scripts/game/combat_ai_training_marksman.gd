f809b11d50660510b7618f1ed5b0305799f41f12
eretenkodaniil16-rgb <eretenkodaniil16@gmail.com>
2026-07-30 13:43:30 +0300

    fix: разрешить выбирать стрелка целью до активации боя

--- scripts/game/combat_ai_training_marksman.gd
+++ scripts/game/combat_ai_training_marksman.gd
@@ -2,6 +2,10 @@ class_name CombatAiTrainingMarksman
 extends "res://scripts/game/stealth_patrol_observer.gd"
 
 
+func is_player_targetable() -> bool:
+	return not defeated
+
+
 func enter_combat_hostile() -> void:
 	if not is_combat_participant_active():
 		get_tree().call_group("stealth_world", "activate_tactical_training_squad")
