64d77f2654a83f77ff576dcb9f821c72af1eb42a
eretenkodaniil16-rgb <eretenkodaniil16@gmail.com>
2026-07-30 11:23:53 +0300

    fix: восстановить корректный GDScript runtime планов

--- scripts/game/game_squad_tactical_plans_runtime.gd
+++ scripts/game/game_squad_tactical_plans_runtime.gd
@@ -53,8 +53,8 @@ func _enrich_advanced_context(
 		_squad_assignment_by_actor.erase(actor_id)
 		return
 	var actor_ids: Array[String] = []
-for member_id_value: Variant in squad_context.get("member_ids", []) as Array:
-    actor_ids.append(str(member_id_value))
+	for member_id_value: Variant in squad_context.get("member_ids", []) as Array:
+		actor_ids.append(str(member_id_value))
 	var actor_index: int = maxi(actor_ids.find(actor_id), 0)
 	var assignment: Dictionary = _squad_plans.get_actor_assignment(
 		squad_id,
