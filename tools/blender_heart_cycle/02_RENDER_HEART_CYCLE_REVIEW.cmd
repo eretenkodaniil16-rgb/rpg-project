292b855adb2d09cd74632bde520f3268b481b11b
eretenkodaniil16-rgb <eretenkodaniil16@gmail.com>
2026-08-05 02:04:21 +0300

    feat(blender): add one-click heart animation review render

---
+++ tools/blender_heart_cycle/02_RENDER_HEART_CYCLE_REVIEW.cmd
@@ -0,0 +1,5 @@
+@echo off
+setlocal
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dprun_blender_heart_cycle.ps1" -RenderAnimation -AnimationResolution 360 -SampleStep 2
+if errorlevel 1 pause
+endlocal
