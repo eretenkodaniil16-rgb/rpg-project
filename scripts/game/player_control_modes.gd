3e0dd76a1519d8ec941f37a703cb98ec328de704
eretenkodaniil16-rgb <eretenkodaniil16@gmail.com>
2026-08-05 23:24:32 +0300

    feat: отключать ввод героя при управлении союзником

--- scripts/game/player_control_modes.gd
+++ scripts/game/player_control_modes.gd
@@ -1,9 +1,11 @@
 class_name PlayerControlModes
 extends "res://scripts/game/player_combat.gd"
 
+var _party_input_enabled: bool = true
+
 
 func _process_exploration_movement(delta: float) -> void:
-	if GameState.input_locked:
+	if GameState.input_locked or not _party_input_enabled:
 		velocity = Vector2.ZERO
 		return
 	var keyboard_direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -20,6 +22,18 @@ func _process_exploration_movement(delta: float) -> void:
 	# GamePlannedCombat._unhandled_input().
 	_cancel_exploration_click_path()
 	velocity = Vector2.ZERO
+
+
+func set_party_input_enabled(value: bool) -> void:
+	_party_input_enabled = value
+	if not value:
+		velocity = Vector2.ZERO
+		set_mobile_vector(Vector2.ZERO)
+		clear_mobile_facing_input()
+
+
+func is_party_input_enabled() -> bool:
+	return _party_input_enabled
 
 
 func set_exploration_click_path(_world_points: Array[Vector2]) -> void:
